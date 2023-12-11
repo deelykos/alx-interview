e09ef4e73bafff119a163562fd84e6a5eca99541
deelykos <fabianohaz@gmail.com>
2023-12-11 06:41:07 +0100

    pep8 check

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,30 +15,14 @@ def canUnlockAll(boxes):
     Returns:
     - bool: True if all boxes can be opened, False otherwise.
     """
-
-    # Number of boxes
     n = len(boxes)
-
-    # Set to keep track of seen boxes
-    seen_boxes = {0}
-
-    # Set of unseen boxes initially excluding the first box
-    unseen_boxes = set(boxes[0]).difference({0})
-
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
     while len(unseen_boxes) > 0:
-        # Pop a box index from the set of unseen boxes
-        boxIdx = unseen_boxes.pop()
-
-        # Check if the box index is valid
-        if not (0 <= boxIdx < n):
-            continue
-
-        # Check if the box has not been seen
-        if boxIdx not in seen_boxes:
-            # Add keys from the current box to the set of unseen boxes
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-
-            # Mark the current box as seen
-            seen_boxes.add(boxIdx)
-
+            boxIdx = unseen_boxes.pop()
+            if not boxIdx or boxIdx >= n or boxIdx < 0:
+                continue
+            if boxIdx not in seen_boxes:
+                unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+                seen_boxes.add(boxIdx)
     return n == len(seen_boxes)
